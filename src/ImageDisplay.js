f1902159ca1c3c8140d3e78390e6fb3d923d5769
Trọng Đại <thantrongdai94@gmail.com>
2020-09-01 09:49:36 +0700

    update

--- src/ImageDisplay.js
+++ src/ImageDisplay.js
@@ -1,8 +1,9 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import "antd/dist/antd.css";
 import Processor from './Processor';
 
 const fakeData = [{
+    id: 1,
     opacity: 1,
     centerX: 0,
     centerY: 0,
@@ -10,6 +11,7 @@ const fakeData = [{
     url: 'https://res.cloudinary.com/mixtiles/image/fetch/x_0,y_211,w_806,h_806,c_crop/c_thumb,w_472,h_472/https://mixtiles-uploads.s3.amazonaws.com/e66dc9c0cf9b2d334dbbbfdbee696daa_upload_web.jpg'
 },
 {
+    id: 2,
     opacity: 1,
     centerX: 0,
     centerY: 0,
@@ -17,6 +19,7 @@ const fakeData = [{
     url: 'https://res.cloudinary.com/mixtiles/image/fetch/x_542,y_0,w_772,h_772,c_crop/c_thumb,w_472,h_472/https://mixtiles-uploads.s3.amazonaws.com/d0befd65429e47833ebbec7db3816d65_upload_web.jpg'
 },
 {
+    id: 3,
     opacity: 1,
     centerX: 0,
     centerY: 0,
@@ -24,6 +27,7 @@ const fakeData = [{
     url: 'https://res.cloudinary.com/mixtiles/image/fetch/x_357,y_0,w_790,h_790,c_crop/c_thumb,w_472,h_472/https://mixtiles-uploads.s3.amazonaws.com/3a070ecd7541534e05306b0a8f7268b8_upload_web.jpg'
 },
 {
+    id: 4,
     opacity: 1,
     centerX: 0,
     centerY: 0,
@@ -31,6 +35,7 @@ const fakeData = [{
     url: 'https://res.cloudinary.com/mixtiles/image/fetch/x_449,y_17,w_319,h_319,c_crop/c_thumb,w_472,h_472/https://mixtiles-uploads.s3.amazonaws.com/79ce4f447017226077196aec2c943838_upload_web.jpg'
 },
 {
+    id: 5,
     opacity: 1,
     centerX: 0,
     centerY: 0,
@@ -53,131 +58,34 @@ const fakeUrl = [
     }
 ]
 const ImageDisplay = () => {
-    const [opacity, setOpacity] = useState(1);
-    const [centerX, setCenterX] = useState(0);
-    const [centerY, setCenterY] = useState(0);
-    const [scale, setScale] = useState(1);
-    const [isOnClick, setOnClick] = useState(false);
-    const [prePosX, setPrePosX] = useState(0);
-    const [prePosY, setPrePosY] = useState(0);
-    const [showModal, setShowModal] = useState(true);
-    const [showAdjust, setShowAdjust] = useState(true);
+    const [showModal, setShowModal] = useState(false);
+    const [showAdjust, setShowAdjust] = useState(false);
     const [listImage, setListImage] = useState(fakeData);
     const [listBgImg, setListBgImg] = useState(fakeUrl);
     const [imgBg, setImgBg] = useState(fakeUrl[0].url);
     const [changeIdx, setChangeIdx] = useState();
 
-    const onMouseDown = (e) => {
-        setOnClick(true);
-    }
-
-    const onMouseUp = (e) => {
-        setPrePosX(0);
-        setPrePosY(0);
-        setOnClick(false);
-    }
-
-    const onMouseMove = async (posX, posY) => {
-        if (!isOnClick) return;
-        if (!prePosX || !prePosY) {
-            setPrePosX(posX);
-            setPrePosY(posY);
-        }
-        if (prePosX < posX && prePosY == posY) {
-            if (centerY < 300) setCenterY(centerY + 1);
-            setPrePosX(posX);
-            setPrePosY(posY);
-            return;
-        }
-        if (prePosX < posX && prePosY < posY) {
-            if (centerX < 300) setCenterX(centerX + 1);
-            if (centerY < 300) setCenterY(centerY + 1);
-            setPrePosX(posX);
-            setPrePosY(posY);
-            return;
-        }
-        if (prePosX < posX && prePosY >
-            posY) {
-            if (centerX >
-                -300) setCenterX(centerX - 1);
-            if (centerY < 300) setCenterY(centerY + 1);
-            setPrePosX(posX);
-            setPrePosY(posY);
-            return;
-        }
-        if (prePosX >
-            posX && prePosY == posY) {
-            if (centerY >
-                -300) setCenterY(centerY - 1);
-            setPrePosX(posX);
-            setPrePosY(posY);
-            return;
-        }
-        if (prePosX >
-            posX && prePosY < posY) {
-            if (centerX < 300) setCenterX(centerX + 1);
-            if (centerY >
-                -300) setCenterY(centerY - 1);
-            setPrePosX(posX);
-            setPrePosY(posY);
-            return;
-        }
-        if (prePosX >
-            posX && prePosY >
-            posY) {
-            if (centerX >
-                -300) setCenterX(centerX - 1);
-            if (centerY >
-                -300) setCenterY(centerY - 1);
-            setPrePosX(posX);
-            setPrePosY(posY);
-            return;
-        }
-        if (prePosX == posX && prePosY >
-            posY) {
-            if (centerX >
-                -300) setCenterX(centerX - 1);
-            setPrePosX(posX);
-            return;
-        }
-        if (prePosX == posX && prePosY < posY) {
-            if (centerX < 300) setCenterX(centerX + 1);
-            setPrePosX(posX);
-            return;
-        }
-    }
-
-    const onScroll = (e) => {
-        if (e.deltaY >
-            0 && scale >
-            1) {
-            setScale(scale - 0.5);
-        }
-        if (e.deltaY < 0 && scale < 5) {
-            setScale(scale + 0.5);
-        }
-    }
-
     const onClickShowModal = (e, idx) => {
         e.preventDefault();
-        setShowModal(false);
-        setChangeIdx(idx);              
+        setShowModal(true);
+        console.log('idx', idx)
+        setChangeIdx(idx);
     }
 
     const onClickHiddenModal = (e) => {
         e.preventDefault();
-        setShowModal(true);
+        setShowModal(false);
     }
 
     const onClickShowAdjust = (e) => {
         e.preventDefault();
-        setShowAdjust(false);
-        setShowModal(true);
+        setShowAdjust(true);
+        setShowModal(false);
     }
 
     const onClickHiddenAdjust = (e) => {
         e.preventDefault();
-        setShowAdjust(true);
+        setShowAdjust(false);
     }
 
     const setBackground = (e, idx) => {
@@ -189,6 +97,15 @@ const ImageDisplay = () => {
         let listTemp = [...listImage];
         listTemp[idx] = img;
         setListImage(listTemp);
+        setShowAdjust(false);
+    }
+
+    const removeImg = (e) => {
+        e.preventDefault();
+        let listTemp = [...listImage];
+        listTemp = listTemp.filter(x => x.id != changeIdx);
+        setListImage(listTemp);
+        setShowModal(false);
     }
 
     return (
@@ -196,7 +113,7 @@ const ImageDisplay = () => {
             <div id="review-order-page" className="review-order-page filter-original" style={{ height: '300px' }}>
                 <div className="TilesStrip">
                     {listBgImg && listBgImg.map((item, idx) => {
-                        return <div id={`tile-${idx}`} className="tile" onClick={e => setBackground(e, idx)}>
+                        return <div id={`tile-${idx}`} key={idx} className="tile" onClick={e => setBackground(e, idx)}>
                             <div className="tile-base" />
                             <div className="preview frameless">
                                 <img alt="" className="preview-image" style={{ opacity: 1, width: '100%' }} />
@@ -206,68 +123,8 @@ const ImageDisplay = () => {
                             </div>
                         </div>
                     })}
-
-                    <div role="presentation" className="MuiDrawer-root MuiDrawer-modal" style={{ position: 'fixed', zIndex: 1300, right: '0px', bottom: '0px', top: '0px', left: '0px' }} hidden>
-                        <div className="MuiBackdrop-root" aria-hidden="true" style={{ opacity: 1, transition: 'opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms' }} />
-                        <div tabIndex={0} data-test="sentinelStart" />
-                        <div className="MuiPaper-root MuiDrawer-paper adjust-tile-drawer MuiDrawer-paperAnchorBottom MuiPaper-elevation16" tabIndex={-1} style={{ transform: 'none', transition: 'transform 225ms cubic-bezier(0, 0, 0.2, 1) 0ms' }}>
-                            <div className="adjust-tile-buttons">
-                                <div className="dialog-button no-border">
-                                    Adjust</div>
-                                <div className="dialog-button highlighted">
-                                    Remove</div>
-                                <div className="dialog-button gray">
-                                    Dismiss</div>
-                            </div>
-                        </div>
-                        <div tabIndex={0} data-test="sentinelEnd" />
-                    </div>
-                    <div className="App" hidden>
-                        <div role="presentation" className="MuiDialog-root mixtiles-dialog-root" style={{ position: 'fixed', zIndex: 1300, right: '0px', bottom: '0px', top: '0px', left: '0px' }}>
-                            <div className="MuiBackdrop-root mixtiles-backdrop" aria-hidden="true" style={{ opacity: 1, transition: 'opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms' }} />
-                            <div tabIndex={0} data-test="sentinelStart" />
-                            <div className="MuiDialog-container MuiDialog-scrollPaper" role="none presentation" tabIndex={-1} style={{ opacity: 1, transform: 'none', transition: 'opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, transform 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms' }}>
-                                <div className="MuiPaper-root MuiDialog-paper mixtiles-dialog-paper MuiDialog-paperScrollPaper MuiDialog-paperWidthSm MuiPaper-elevation24 MuiPaper-rounded" role="dialog">
-                                    <div className="tile-cropper">
-                                        <div className="top-bar-container no-bottom-margin">
-                                            <div className="top-bar ">
-                                                <div className="left-comp">
-                                                    <div className="CloseButton">
-                                                        <img src="/images/icons/xIcon.svg" />
-                                                    </div>
-                                                </div>
-                                                <div className="title ">
-                                                    Adjust</div>
-                                                <div className="right-comp">
-                                                    <div className="DoneButton">
-                                                        Done</div>
-                                                </div>
-                                            </div>
-                                            <div className="bottom-comp" />
-                                        </div>
-                                        <div className="cropper-content filter-original">
-                                            <div className="cropper-text">
-                                                PINCH AND ZOOM</div>
-                                            <div data-testid="container" className="cropper-container-style css-1dkwqii" style={{ textAlign: 'center' }}>
-                                                <img alt="" className="cropper-image-style css-ebdd77" src="https://res.cloudinary.com/mixtiles/image/fetch/q_auto/https://mixtiles-uploads.s3.amazonaws.com/e66dc9c0cf9b2d334dbbbfdbee696daa_upload_web.jpg" style={{ width: '283px', transform: 'translate(0px, 0px) rotate(0deg) scale(1)' }} />
-                                                <div data-testid="cropper" className="cropper-area-style css-nikas5" style={{ width: '283px', height: '283px' }} />
-                                            </div>
-                                            <div className="frame-container">
-                                                <div className="tile-base transparent" />
-                                                <div className="TileFrame">
-                                                    <img className="frame" src="/images/frames/none.svg" />
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div tabIndex={0} data-test="sentinelEnd" />
-                        </div>
-                    </div>
                 </div>
             </div>
-
             <div id="review-order-page" className="review-order-page filter-original">
                 <div className="content">
                     <div className="TilesStrip">
@@ -291,20 +148,19 @@ const ImageDisplay = () => {
                                         <img className="cloud-icon" src="/images/uploader/instagram.svg" />
                                         <img className="cloud-icon smaller" src="/images/uploader/googledrive.svg" />
                                     </div>
-                                    <div className="text">
-                                        Choose from Online Services</div>
+                                    <div className="text">Choose from Online Services</div>
                                 </div>
                             </div>
                         </div>
                         {
                             listImage && listImage.map((item, idx) => {
-                                return <div id={`tile-${idx}`} className="tile">
+                                return <div id={`tile-${idx}`} key={idx} className="tile">
                                     <div className="tile-base" />
                                     <div className="preview frameless">
-                                        <img alt="" className="preview-image" src={item.url} style={{ opacity: 1, width: '100%' }} />
+                                        <img alt="" className="preview-image" src={item.url} style={{ scale: item.scale, width: '100%', transform: `translate(${item.centerY}px, ${item.centerX}px) rotate(0deg) scale(${item.scale})` }} />
                                     </div>
                                     <div className="TileFrame" onClick={e =>
-                                        onClickShowModal(e, idx)}>
+                                        onClickShowModal(e, item.id)}>
                                         <img className="frame" src={imgBg} />
                                     </div>
                                 </div>
@@ -335,41 +191,43 @@ const ImageDisplay = () => {
                                         <img className="cloud-icon" src="/images/uploader/instagram.svg" />
                                         <img className="cloud-icon smaller" src="/images/uploader/googledrive.svg" />
                                     </div>
-                                    <div className="text">
-                                        Choose from Online Services</div>
+                                    <div className="text">Choose from Online Services</div>
                                 </div>
                             </div>
-                        </div>
-
-                        <div hidden={showModal} role="presentation" className="MuiDrawer-root MuiDrawer-modal" style={{ position: 'fixed', zIndex: 1300, right: '0px', bottom: '0px', top: '0px', left: '0px' }}>
-                            <div className="MuiBackdrop-root" aria-hidden="true" style={{ opacity: 1, transition: 'opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms' }} />
-                            <div tabIndex={0} data-test="sentinelStart" />
-                            <div className="MuiPaper-root MuiDrawer-paper adjust-tile-drawer MuiDrawer-paperAnchorBottom MuiPaper-elevation16" tabIndex={-1} style={{ transform: 'none', transition: 'transform 225ms cubic-bezier(0, 0, 0.2, 1) 0ms' }}>
-                                <div className="adjust-tile-buttons">
-                                    <div className="dialog-button no-border" onClick={e =>
-                                        onClickShowAdjust(e)}>
-                                        Adjust</div>
-                                    <div className="dialog-button highlighted">
-                                        Remove</div>
-                                    <div className="dialog-button gray" onClick={e =>
-                                        onClickHiddenModal(e)}>
-                                        Dismiss</div>
-                                </div>
-                            </div>
-                            <div tabIndex={0} data-test="sentinelEnd" />
                         </div>
                         {
-                            listImage && changeIdx > -1 && <Processor
-                                showAdjust={showAdjust}
-                                onClickHiddenAdjust={onClickHiddenAdjust}
-                                changeImage={changeImage}
-                                changeIdx={changeIdx}
-                                listImage={listImage}
-                            />
+                            showModal ? <>
+                                <div role="presentation" className="MuiDialog-root mixtiles-dialog-root" style={{ position: 'fixed', zIndex: 1300, right: '0px', bottom: '0px', top: '0px', left: '0px' }}>
+                                    <div className="MuiBackdrop-root" hidden={showModal} role="presentation" aria-hidden="true" style={{ opacity: 1, transition: 'opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms' }} />
+                                    <div tabIndex={0} data-test="sentinelStart" />
+                                    <div className="MuiPaper-root MuiDrawer-paper adjust-tile-drawer MuiDrawer-paperAnchorBottom MuiPaper-elevation16" tabIndex={-1} style={{ transform: 'none', transition: 'transform 225ms cubic-bezier(0, 0, 0.2, 1) 0ms' }}>
+                                        <div className="adjust-tile-buttons">
+                                            <div className="dialog-button no-border" onClick={e =>
+                                                onClickShowAdjust(e)}>
+                                                Tùy chỉnh</div>
+                                            <div className="dialog-button highlighted" onClick={e => removeImg(e)}>
+                                                Xóa bỏ</div>
+                                            <div className="dialog-button gray" onClick={e =>
+                                                onClickHiddenModal(e)}>
+                                                Bỏ qua</div>
+                                        </div>
+                                    </div>
+                                    <div tabIndex={0} data-test="sentinelEnd" />
+                                </div>
+                            </> : null
                         }
                     </div>
                 </div>
             </div>
+            {
+                listImage && changeIdx >= 0 && showAdjust && <Processor
+                    showAdjust={showAdjust}
+                    onClickHiddenAdjust={onClickHiddenAdjust}
+                    changeImage={changeImage}
+                    changeIdx={changeIdx}
+                    listImage={listImage}
+                />
+            }
         </>
     );
 }
--- src/Processor.js
+++ src/Processor.js
@@ -11,11 +11,11 @@ const Processor = (props) => {
     const imgRef = useRef();
 
     useEffect(() => {
-        // setCenterX(props.listImage[props.changeIdx].centerX);
-        // setCenterX(props.listImage[props.changeIdx].centerY);
-        // setCenterX(props.listImage[props.changeIdx].opacity);
-        // setCenterX(props.listImage[props.changeIdx].scale);
-    }, [])
+        setCenterX(props.listImage[props.changeIdx].centerX);
+        setCenterY(props.listImage[props.changeIdx].centerY);
+        setScale(props.listImage[props.changeIdx].scale);
+        setOpacity(props.listImage[props.changeIdx].opacity);
+    }, []);
 
     const onMouseDown = (e) => {
         setOnClick(true);
@@ -98,7 +98,6 @@ const Processor = (props) => {
     }
 
     const onScroll = (e) => {
-        console.log('e', e)
         e.preventDefault();
         if (e.deltaY >
             0 && scale >
@@ -111,9 +110,9 @@ const Processor = (props) => {
     }
 
     return (
-        <div className="App" hidden={props.showAdjust}>
+        <>
             <div role="presentation" className="MuiDialog-root mixtiles-dialog-root" style={{ position: 'fixed', zIndex: 1300, right: '0px', bottom: '0px', top: '0px', left: '0px' }}>
-                <div className="MuiBackdrop-root mixtiles-backdrop" aria-hidden="true" style={{ opacity: 1, transition: 'opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms' }} />
+                <div className="MuiBackdrop-root mixtiles-backdrop" hidden={props.showAdjust} aria-hidden="true" style={{ opacity: 1, transition: 'opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms' }} />
                 <div tabIndex={0} data-test="sentinelStart" />
                 <div className="MuiDialog-container MuiDialog-scrollPaper" role="none presentation" tabIndex={-1} style={{ opacity: 1, transform: 'none', transition: 'opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, transform 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms' }}>
                     <div className="MuiPaper-root MuiDialog-paper mixtiles-dialog-paper MuiDialog-paperScrollPaper MuiDialog-paperWidthSm MuiPaper-elevation24 MuiPaper-rounded" role="dialog">
@@ -125,9 +124,8 @@ const Processor = (props) => {
                                             <img src="/images/icons/xIcon.svg" />
                                         </div>
                                     </div>
-                                    <div className="title ">
-                                        Adjust</div>
-                                    <div className="right-comp" >
+                                    <div className="title ">Adjust</div>
+                                    <div className="right-comp" onClick={e => props.changeImage(props.changeIdx, { scale, centerX, centerY, opacity, url: props.listImage[props.changeIdx].url })}>
                                         <div className="DoneButton">Done</div>
                                     </div>
                                 </div>
@@ -138,7 +136,7 @@ const Processor = (props) => {
                                     <div className="cropper-text">
                                         PINCH AND ZOOM</div>
                                     <div data-testid="container"
-                                        style={{ textAlign: 'center' }}
+                                        style={{ textAlign: 'center', top: '100px', bottom: '100px' }}
                                         className="cropper-container-style css-1dkwqii"
                                         onMouseDown={e =>
                                             onMouseDown(e)}
@@ -148,7 +146,6 @@ const Processor = (props) => {
                                             onMouseUp(e)}
                                         onWheel={e =>
                                             onScroll(e)}>
-                                        {console.log('props.listImage[props.changeIdx]', props.listImage[props.changeIdx])}
                                         <img
                                             alt="" className="cropper-image-style css-ebdd77"
                                             src={props.listImage[props.changeIdx] ? props.listImage[props.changeIdx].url : ''}
@@ -157,9 +154,7 @@ const Processor = (props) => {
                                                 onMouseUp(e)}
                                             ref={imgRef}
                                         />
-                                        <div data-testid="cropper" className="cropper-area-style css-nikas5" style={{ width: '283px', height: '283px' }}
-                                            onScroll={e =>
-                                                onScroll(e)} />
+                                        <div data-testid="cropper" className="cropper-area-style css-nikas5" style={{ width: '283px', height: '283px' }} />
                                     </div>
                                     <div className="frame-container">
                                         <div className="tile-base transparent" />
@@ -178,7 +173,7 @@ const Processor = (props) => {
                 </div>
                 <div tabIndex={0} data-test="sentinelEnd" />
             </div>
-        </div>
+        </>
     );
 }
 
